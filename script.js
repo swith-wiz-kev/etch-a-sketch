68d60a17700578c995be9a0cb5689f715cb8109f
kev <ddrain10@gmail.com>
2022-11-24 20:46:58 +0800

    Add create squares function

---
+++ script.js
@@ -0,0 +1,25 @@
+function createGrid(sideLength) {
+    const container = document.querySelector('.grid.container');
+    // delete all elements inside container
+    const gridSquares = document.createElement('div');
+    gridSquares.className = "grid squares";
+    const width = Math.floor((800-sideLength) / sideLength);
+    const offset = Math.floor((800 - (width+1)*sideLength)/2);
+    gridSquares.style.width = width + 'px';
+    gridSquares.style.height = width + 'px';
+    let topPosition = 0;
+    let leftPosition = 0;
+    for (let i = 0; i < sideLength; i++) {
+        leftPosition =  i*(width+1)+offset;
+        for (let j = 0; j < sideLength; j++) {
+            topPosition =  j*(width+1)+offset;
+            const clone = gridSquares.cloneNode(false);
+            clone.style.top = topPosition + 'px';
+            clone.style.left = leftPosition + 'px';
+            container.appendChild(clone);
+        }
+        
+    }
+}
+
+createGrid(12);
